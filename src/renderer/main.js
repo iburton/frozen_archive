622572d16c212ac59f2d9a8c118a4b5bd8cced37
Ira Burton <ira.burton@gmail.com>
2019-11-27 11:13:06 -0700

    :tada: Initial commit

---
+++ src/renderer/main.js
@@ -0,0 +1,31 @@
+// import the styles
+import 'bulma-pro/bulma.sass'
+import { ipcRenderer } from 'electron'
+import 'material-design-icons/iconfont/material-icons.css'
+import Vue from 'vue'
+import App from './App.vue'
+import './assets/style/animations.scss'
+import './assets/style/main.scss'
+import router from './router/index'
+import store from './store/index'
+
+const isDev = process.env.NODE_ENV === 'development'
+
+Vue.config.devtools = isDev
+Vue.config.performance = isDev
+Vue.config.productionTip = isDev
+
+// tslint:disable-next-line: no-unused-expression
+new Vue({
+  el: '#app',
+  router,
+  store,
+  render: h => h(App),
+})
+
+// handle menu event updates from main script
+ipcRenderer.on('change-view', (event, data) => {
+  if (data.route) {
+    router.push(data.route)
+  }
+})
